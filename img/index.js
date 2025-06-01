85b0ae4692fdd41890550317519bf77355d51075
cord70 <emd.kornienko@gmail.com>
2025-06-01 15:36:48 +0300

    script to index.html set inline

--- img/index.js
+++ img/index.js
@@ -7,9 +7,9 @@ var url = Link ? Link.href : null;
 if(url!=null && url.endsWith(home))
   {
     var dir= url.slice(0, -home.length);
-    dir= dir.slice(Math.max(dir.lastIndexOf('.'),dir.lastIndexOf('/'))+1);
+    dir= dir.slice(Math.max(dir.lastIndexOf('.'), dir.lastIndexOf('/'))+1);
     url= window.location.href;     
-    if(url.endsWith(dir+"/"))	url= url.slice(0,-1);
+    if(url.endsWith("/"))	url= url.slice(0, -1);
     if(url.endsWith(dir))	window.location.href= url+home; 
   }
 
