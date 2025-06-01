cace3982cf66f11afff86d908c3bf7a274b96a4e
cord70 <emd.kornienko@gmail.com>
2025-06-01 22:15:19 +0300

    removed pagelist from 404

--- img/index.js
+++ img/index.js
@@ -1,7 +1,7 @@
 // переход на ../index.html
   
 const Link = document.querySelector('link[rel="canonical"]');
-const home= "/index.html";
+const home= '/index.html';
 var url = Link ? Link.href : null;
   
 if(url!=null && url.endsWith(home))
@@ -9,7 +9,7 @@ if(url!=null && url.endsWith(home))
     var dir= url.slice(0, -home.length);
     dir= dir.slice(Math.max(dir.lastIndexOf('.'), dir.lastIndexOf('/'))+1);
     url= window.location.href;     
-    if(url.endsWith("/"))	url= url.slice(0, -1);
+    if(url.endsWith('/'))	url= url.slice(0, -1);
     if(url.endsWith(dir))	window.location.href= url+home; 
   }
 
