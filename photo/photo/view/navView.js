f9d685d7124d37181669f6f6e55026822ab78444
cord70 <emd.kornienko@gmail.com>
2025-12-12 12:35:15 +0200

    water etc - -en.html -> jpg

--- photo/photo/view/navView.js
+++ photo/photo/view/navView.js
@@ -5,3 +5,11 @@ var menu=
   <p><a href="steel-spring.html"> Пружина</a></p>\
 <h4><a href="../water/index.html"> Вода</a></h4>\
 <h4><a href="../flame/index.html"> Огонь</a></h4>';
+
+var menuen=
+'<h4><a href="en.html"> View</a></h4>\
+  <p><a href="forest-en.html"> Forest</a></p>\
+<h4> Still</h4>\
+  <p><a href="steel-spring-en.html"> Spring</a></p>\
+<h4><a href="../water/en.html"> Water</a></h4>\
+<h4><a href="../flame/en.html"> Flame</a></h4>';
